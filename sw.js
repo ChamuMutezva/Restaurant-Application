ed37d22d43103afff59d00c651e2a4fb6a35682b
Chamunorwa Mutezva <ckmutezva@gmail.com>
2018-10-08 21:31:46 +0200

    webworker error corrections

--- sw.js
+++ sw.js
@@ -1,56 +1,63 @@
 console.log("Service worker: registered");
- let CACHE_NAME = 'restaurant-v1';
+let CACHE_NAME = 'restaurant-v1';
 
-self.addEventListener('Install', function(e){
-e.waitUntil(
-	caches.open(CACHE_NAME).then(function(cache){
-		return cache.addAll([
-'./',
-'./index.html',
-'./restaurant.html',
-'./css/styles.css',
-'./css/mobileCss.css',
-'./css/laptop.css',
-'./js/dbhelper.js',
-'.data/restaurant.json',
-'./js/main.js',
-'./js/restaurant_info.js',
-'./js/sw_register.js',
-'./img/1.jpg',
-'./img/2.jpg',
-'./img/3.jpg',
-'./img/4.jpg',
-'./img/5.jpg',
-'./img/6.jpg',
-'./img/7.jpg',
-'./img/8.jpg',
-'./img/10.jpg'
+self.addEventListener('Install', function (event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			return cache.addAll([
+				'./',
+				'./index.html',
+				'./restaurant.html',
+				'./css/styles.css',
+				'./css/mobileCss.css',
+				'./css/laptop.css',
+				'./js/dbhelper.js',
+				'.data/restaurant.json',
+				'./js/main.js',
+				'./js/restaurant_info.js',
+				'./js/sw_register.js',
+				'./img/1.jpg',
+				'./img/2.jpg',
+				'./img/3.jpg',
+				'./img/4.jpg',
+				'./img/5.jpg',
+				'./img/6.jpg',
+				'./img/7.jpg',
+				'./img/8.jpg',
+				'./img/10.jpg'
 
 			]);
-	})
+		})
 	);
 });
 
-self.addEventListener('fetch', function(e){
-	e.respondWith(
-		caches.match(e.request).then(function(response){
-		if (response){
-			console.log('Found', e.request, ' in cache');
-			return response;
-		}
-		else {
-			console.log('Cannot find ' , e.request, ' in cache, FETCHING!');
-			return fetch(e.request).then(function(response){
-				caches.open(CACHE_NAME).then(function(cache){
-					cache.put(e.request, response);
-				})
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function (response) {
+			if (response) {
+				//	console.log('Found', e.request, ' in cache');
 				return response;
-			})
-			.catch(function(err){
-				console.error(err);
-			})
-		}	
-		})
+			}
+			var fetchRequest = event.request.clone();
 
-		);
-})
+			return fetch(fetchRequest).then(
+				function(response){
+					//check if a valid response has been received
+					if(!response || response.status !== 200 || response.type !== 'basic'){
+						return response;
+					}
+					var responseToCache = response.clone();
+
+					caches.open(CACHE_NAME)
+					.then(function(cache){
+						cache.put(event.request, responseToCache);
+					});
+					return response;
+				}
+			);
+			//return fetch(event.request);
+		})
+	);
+
+});
